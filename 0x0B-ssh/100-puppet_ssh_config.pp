bf35a9ccbd6e28f71867b5c9d552679558d28e65
Ogochukwu GloriaMary Nwanochiri <113646633+Ogoobaby@users.noreply.github.com>
2023-04-24 05:40:28 +0100

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,16 @@
-#!/usr/bin/env bash
-# set up your client SSH configuration file so that you can connect to a server without typing a password.
-file_line {'Turn off passwd auth':
-  ensure => 'present',
+# Seting up my client config file
+include stdlib
+
+file_line { 'Turn off passwd auth':
+  ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => '    PasswordAuthentication no',
-  }
-file_line { 'Declare identity file':
-  ensure => 'present',
+  replace => true,
+}
+
+file_line { 'Delare identity file':
+  ensure => present,
   path   => '/etc/ssh/ssh_config',
-  line   => '    IdentityFile ~/.ssh/school',
-  }
+  line   => '     IdentityFile ~/.ssh/school',
+  replace => true,
+}
